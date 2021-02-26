a50ff91d89917d72754cca2d3671d1b045b82b88
Muhammad Faris Bin Abdul Bashir <71578500+farisbashir@users.noreply.github.com>
2021-02-27 01:47:49 +0800

    Update header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -12,7 +12,7 @@ if(Session::has('user'))
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header" >
-      <a class="navbar-brand" href="/">Supplycart Task</a>
+      <a class="navbar-brand" href="/">E-commerce</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
